85961eeb2bcc8e23ef62d83517cbc067b5762091
JayA_BharaTh <jayabharath.g.01@gmail.com>
2026-07-14 07:11:06 +0000

    Commit all updates on v2.0

--- aws/s3/s3connection.py
+++ aws/s3/s3connection.py
@@ -2,8 +2,8 @@ import boto3
 
 s3 = boto3.client(
     "s3",
-    aws_access_key_id="AKIAWVLQESP5PBVC7TOB",
-    aws_secret_access_key="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",
+    aws_access_key_id="",
+    aws_secret_access_key="",
     region_name="ap-south-1"
 )
 
